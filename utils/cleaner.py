5182fb4583a1c6b2abaaf9b31d0ff2075ff6121c
Huytranxuan <74607304+TranXuanHuy267@users.noreply.github.com>
2023-11-18 16:07:01 +0700

    create text cleaner

---
+++ utils/cleaner.py
@@ -0,0 +1,14 @@
+import re
+import math
+
+def cleaning(text):
+    if isinstance(text, str): 
+        text = text.lower()
+        text = re.sub(r'[^ ,.?!a-z0-9àáảãạăắằẳẵặâấầẩẫậèéẻẽẹêếềểễệđìíỉĩịòóỏõọôốồổỗộơớờởỡợùúủũụưứừửữựỳýỷỹỵ]', '', text)
+        text = re.sub(r'[.!?]+', '.', text)
+        text = re.sub(r'([,.!?])\1+', r'\1', text)
+        text = re.sub(r'[,]+', ' , ', text)
+        text = re.sub(r'[.]+', ' . ', text)
+        text = re.sub(r'([ ])\1+', r'\1', text)
+        return text
+    return text
